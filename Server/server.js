da96690413b26f11c8282d04e065c386ad687de8
mahesh_war <maheshreddyraya@gmail.com>
2021-10-02 16:17:59 +0530

    Connected to Database

--- Server/server.js
+++ Server/server.js
@@ -1,30 +1,45 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
 const mongoose = require("mongoose");
-const { MONGODB_URL } = require("./config.js");
 
-const typeDefss = gql`
+const Post = require("./models/Post");
+const { MONGODB_URL, PORT } = require("./config.js");
+
+const typeDefs = gql`
+	type Post {
+		id: ID!
+		body: String!
+		userName: String!
+		createdAt: String!
+	}
 	type Query {
-		sayHi: String!
+		getPosts: [Post]
 	}
 `;
 
 const resolvers = {
 	Query: {
-		sayHi: () => "Hello!!!!!",
+		async getPosts() {
+			try {
+				const posts = await Post.find();
+				return posts;
+			} catch (err) {
+				throw new Error(err);
+			}
+		},
 	},
 };
 
 const server = new ApolloServer({
-	typeDefs: typeDefss,
+	typeDefs,
 	resolvers,
 });
 
 mongoose
 	.connect(MONGODB_URL, { useNewUrlParser: true })
 	.then(() => {
-    console.log("Connected to Database")
-		return server.listen({ port: 5000 });
+		console.log("Connected to Database");
+		return server.listen(process.env.PORT || PORT);
 	})
 	.then((res) => {
 		console.log(`Server running at ${res.url}`);
